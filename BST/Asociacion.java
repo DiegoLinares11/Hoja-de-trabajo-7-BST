7c81bf1bd49fa185b5286a2254fc12ccfd6c3fe2
DiegoLinares11 <lin221256@uvg.edu.gt>
2023-03-23 13:37:05 -0600

    Crear Nodo

--- BST/Asociacion.java
+++ BST/Asociacion.java
@@ -21,3 +21,4 @@ public class Asociacion<K, V> {
 	}
 
 }
+//sss
--- BST/Nodo.java
+++ BST/Nodo.java
@@ -1,4 +1,21 @@
 package BST;
-public class Nodo {
-    
+
+public class Nodo<E>{
+	
+	private Asociacion<String, String> as;
+	Nodo<E> right;
+	Nodo<E> left;
+	
+	
+	public Nodo(String ingles, String espanol) {
+		as = new Asociacion<String, String>(ingles, espanol);
+		
+	}
+	
+	public Asociacion<String, String> getLast(){
+		return as;
+		
+	}
+
+	
 }
