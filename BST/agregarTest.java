501616f19620d9925db3a49a280d5ef8da672eaf
DiegoLinares11 <lin221256@uvg.edu.gt>
2023-03-24 22:57:33 -0600

    Elimine factory, no utilizada en el programa

--- BST/agregarTest.java
+++ BST/agregarTest.java
@@ -1,23 +1,26 @@
 /*
  * ESTO FUNCIONA EN INTELLIJ NADA MAS, MALA CONFIGURACION DE MI .IML
- * 
+ * ADJUNTO FOTOS EN EL DOC CON EL LINK DEL REPO.
  * package BST;
- * 
  * import static org.junit.jupiter.api.Assertions.*;
  * 
  * import org.junit.jupiter.api.Test;
  * 
- * class encontrarTest {
+ * class putTest {
  * 
  * @Test
  * void test() {
  * BinaryTree<Nodo<AsociacionBST<String, String>>> tree = new
- * BinaryTree<Nodo<AsociacionBST<String, String>>>();
+ * BinaryTree<Nodo<AsociacionBST<String,String>>>();
+ * boolean prueba;
  * tree.addNode("Hello", "Hola");
- * String palabra;
- * palabra = tree.find("Hello");
- * assertEquals("Hola", palabra);
- * 
+ * if(tree.raiz != null) {
+ * prueba = true;
+ * }
+ * else {
+ * prueba = false;
+ * }
+ * assertEquals(true, prueba);
  * }
  * 
  * }
